60f1d43c762b4fdf92c5b3bc3f6142b2bb87ca39
Sawan <sawankushwah457@gmail.com>
2024-10-06 17:53:08 +0530

    Added club page SK

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 // import Image from "next/image";
+import Club from "./components/Club";
 import Footer from "./components/Footer";
 import TechzMagzine from "./components/TechzMagzine";
 
@@ -8,6 +9,7 @@ export default function Home() {
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         Welcome to RCCTechz Website Revamp
       </div>
+      <Club />
       <TechzMagzine />
       <Footer />
     </>
