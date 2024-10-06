c5c75c95c26410bbf9c00df9a7c2c1ecc1cc1525
suraj sakhare <sakharesuraj10@gmail.com>
2024-10-06 23:05:14 +0530

    Project page done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,10 @@
-
+import FAQ from "@/components/Faq";
+import Club from "@/components/Club";
+import Footer from "@/components/Footer";
+import TechzMagzine from "@/components/TechzMagzine";
 import OurWork from "@/components/OurWork";
+import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div>
-      <OurWork/>
-    </div>
-// import Image from "next/image";
-import FAQ from "./components/Faq";
-=======
-import Club from "./components/Club";
-import Footer from "./components/Footer";
-import TechzMagzine from "./components/TechzMagzine";
 
 export default function Home() {
   return (
@@ -19,6 +12,7 @@ export default function Home() {
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         Welcome to RCCTechz Website Revamp
       </div>
+      <OurWork/>
       <Club />
       <TechzMagzine />
       <FAQ />
