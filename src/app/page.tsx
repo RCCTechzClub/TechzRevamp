94217efc1506b367a798b6b7a3b9f5fca2690523
dev129 <76431221+dev129@users.noreply.github.com>
2024-10-07 12:24:34 +0530

    Navbar Design and setup
    This commit includes all the necessary sretup particulary i have used daisyUI for navbar designing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+// src/pages/Home.js
+import React from "react";
+import Navbar from "../app/components/Navbar";
+import '../styles.css';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Welcome to RCCTechz Website Revamp
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
