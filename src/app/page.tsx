32b717ca5f0d91034e619f6ff13c145f2d7d4240
abhi <abhi@mailserver>
2024-10-08 15:48:59 +0545

    navbar addition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
+    <Navbar />
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         Welcome to RCCTechz Website Revamp
       </div>
