a571baa976cc21b23cd5f907e180ec6d5a663afe
abhi <abhi@mailserver>
2024-10-08 15:45:40 +0545

    fix damaged files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 
-// src/pages/Home.js
 import React from "react";
 import Navbar from "../app/components/Navbar";
 import '../styles.css';
-=======
 import FAQ from "@/components/Faq";
 import Club from "@/components/Club";
 import Footer from "@/components/Footer";
@@ -16,10 +14,6 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-
-      <Navbar />
-    </>
-
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         Welcome to RCCTechz Website Revamp
       </div>
@@ -29,6 +23,5 @@ export default function Home() {
       <FAQ />
       <Footer />
     </>
-
   );
 }
