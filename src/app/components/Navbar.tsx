525cbfed900b8980df71e76493c01591eeca515a
RishabxBhardwaj <rishabcgc@gmail.com>
2024-10-17 23:20:42 +0530

    Improve Navbar styles and make it responsive

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,7 +1,6 @@
 'use client';
 import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
-
 import Logo from '../public/Logo.png';
 import '@/styles.css';
 import { ModeToggle } from './button-mode';
@@ -13,20 +12,19 @@ const Navbar = () => {
   useEffect(() => {
     const savedTheme = localStorage.getItem('theme');
     if (savedTheme) {
-      setIsDarkMode(savedTheme === 'dark');
-      document.body.classList.toggle('dark-mode', savedTheme === 'dark');
+      const darkMode = savedTheme === 'dark';
+      setIsDarkMode(darkMode);
+      document.body.classList.toggle('dark-mode', darkMode);
     }
   }, []);
 
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
-  };
+  const toggleDropdown = () => setIsOpen(prev => !prev);
 
   const toggleDarkMode = () => {
     const newTheme = isDarkMode ? 'light' : 'dark';
     setIsDarkMode(!isDarkMode);
     document.body.classList.toggle('dark-mode', !isDarkMode);
-    localStorage.setItem('theme', newTheme); 
+    localStorage.setItem('theme', newTheme);
   };
 
   const navItems = [
@@ -38,16 +36,17 @@ const Navbar = () => {
   ];
 
   return (
-    <div className={`navbar sticky top-0 start-0 w-full bg-opacity-30 backdrop-blur-md ${isDarkMode ? 'bg-gray-900' : 'bg-gray-200'} text-white`}>
-      <div className="navbar-start flex items-center justify-between w-full">
-        <Image src={Logo} alt='Logo' height={50} width={50} />
-        <div className="navbar-center hidden lg:flex space-x-4">
+    <div className={`navbar sticky top-0 w-full bg-opacity-90 backdrop-blur-md transition duration-300 ease-in-out ${isDarkMode ? 'bg-gray-900' : 'bg-gray-200'} text-white`}>
+      <div className="navbar-start flex items-center justify-between w-full p-4 bg-slate-950">
+        <Image src={Logo} alt='Logo' height={50} width={50} className="cursor-pointer" />
+
+        <div className="navbar-center hidden lg:flex space-x-8">
           <ul className="menu menu-horizontal flex text-sm">
-            {navItems.map((item) => (
+            {navItems.map(item => (
               <li key={item.label}>
                 <a
-                  href={`${item.id}`}
-                  className="block text-lg leading-5 text-gray-200 transition-colors duration-300 p-2 rounded font-extralight"
+                  href={item.id}
+                  className="block text-lg leading-5 text-gray-200 hover:text-orange-400 transition-colors duration-300 p-2 rounded font-extralight"
                 >
                   {item.label}
                 </a>
@@ -55,13 +54,13 @@ const Navbar = () => {
             ))}
           </ul>
         </div>
-        <div className="navbar-end flex items-center gap-5">
-          <a className="btn bg-transparent px-5 text-orange-400 border-orange-400 rounded-3xl hover:bg-orange-400 hover:text-white hover:border-none">Log In</a>
-          <a className="btn border-transparent bg-orange-400 px-5 text-white rounded-full hover:bg-transparent hover:text-orange-400 hover:border-orange-400">Sign Up</a>
-          
+
+        <div className="navbar-end flex items-center gap-4 ">
+          <a className="btn bg-transparent px-5 text-orange-400 border-orange-400 rounded-3xl hover:bg-orange-400 hover:text-white transition-colors duration-300">Log In</a>
+          <a className="btn border-transparent bg-orange-400 px-5 text-white rounded-full hover:bg-transparent hover:text-orange-400 hover:border-orange-400 transition-colors duration-300">Sign Up</a>
           <ModeToggle />
         </div>
-        
+
         <div className="navbar-center lg:hidden">
           <button className="btn btn-ghost" onClick={toggleDropdown}>
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -69,13 +68,13 @@ const Navbar = () => {
             </svg>
           </button>
           {isOpen && (
-            <ul className="menu dropdown-content mt-3 p-2  pt-28 shadow bg-gray-800 rounded-box w-52">
-              {navItems.map((item) => (
+            <ul className="menu dropdown-content mt-3 p-2 pt-2 shadow bg-gray-800 rounded-box w-52">
+              {navItems.map(item => (
                 <li key={item.label}>
                   <a
-                    href={`${item.id}`}
-                    className="block text-lg leading-6 text-gray-200 transition-colors duration-300 p-2 rounded font-extralight"
-                    onClick={() => setIsOpen(false)} 
+                    href={item.id}
+                    className="block text-lg leading-6 text-gray-200 hover:text-orange-400 transition-colors duration-300 p-2 rounded font-extralight"
+                    onClick={() => setIsOpen(false)}
                   >
                     {item.label}
                   </a>
